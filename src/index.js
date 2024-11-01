6e38dbdb5ddb269bc4bfa00dc4609061608b5d7c
Md fareed <fareedzaman501@gmail.com>
2024-11-02 02:50:33 +0530

    db connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
 
 
 /*
 import express from "express"
 const app = express()
-
 
 (async ()=>{
     try{
