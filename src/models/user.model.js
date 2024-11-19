c0e00c12ec53d5b517d57fc83fe63cb666dd78f1
Md fareed <fareedzaman501@gmail.com>
2024-11-19 21:18:12 +0530

    add bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from bcrypt
 
 const userSchema = new Schema(
     {
@@ -50,5 +52,11 @@ const userSchema = new Schema(
     {timestamps: true}
 )
 
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
 
 export const User = mongoose.model("User",userSchema)
