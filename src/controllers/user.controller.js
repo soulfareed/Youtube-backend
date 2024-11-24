63497a1302c301a9ca6e4c1f46063e48ffdac760
Md fareed <fareedzaman501@gmail.com>
2024-11-24 23:36:10 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,25 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "chai ar code",
-  });
+  //get user details from frontend
+  //validation - !empty
+  //check if user already exist: username , email
+  //check for images
+  //upload them to cloudinary, avatar
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  // return res
+
+  const { fullname, email, password, username } = req.body;
+  console.log("email :", email, "password :", password);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
 });
 
 export { registerUser };
