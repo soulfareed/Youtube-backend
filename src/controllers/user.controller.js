1485e5d040f33216d3f47d6a2b5d0d3017014df9
Md fareed <fareedzaman501@gmail.com>
2024-12-10 15:46:40 +0530

    logout user update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,5 +153,26 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler(async (reg, res) => {});
+const logoutUser = asyncHandler(async (reg, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out Successfully"));
+});
 export { registerUser, loginUser, logoutUser };
