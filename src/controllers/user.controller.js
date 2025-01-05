0e4d49923693ba4f6e464c3ad61fb2da7be0efcd
Md fareed <fareedzaman501@gmail.com>
2025-01-06 01:56:43 +0530

    problem in register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -91,7 +91,6 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser) {
     throw ApiError(500, "Something went wrong while registering the user");
   }
-
   return res
     .status(201)
     .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
